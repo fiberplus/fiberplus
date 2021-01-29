de6b2d7665a3aa847134a5b2473517e297fa226b
sachin <sacsand.k@gmail.com>
2021-01-29 16:01:51 +0800

    🚨 Create file global function

--- cmd/config.go
+++ cmd/config.go
@@ -9,15 +9,35 @@ func check(e error) {
 }
 
 // exists returns whether the given file or directory exists
-func exists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
+// func exists(path string) (bool, error) {
+// 	_, err := os.Stat(path)
+// 	if err == nil {
+// 		return true, nil
+// 	}
+// 	if os.IsNotExist(err) {
+// 		return false, nil
+// 	}
+// 	return false, err
+// }
+
+var exists = func(filename string) bool {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return false
 	}
-	if os.IsNotExist(err) {
-		return false, nil
+	return true
+}
+
+func createFile(filePath, content string) (err error) {
+	var f *os.File
+	if f, err = os.Create(filePath); err != nil {
+		return
 	}
-	return false, err
+
+	defer func() { _ = f.Close() }()
+
+	_, err = f.WriteString(content)
+
+	return
 }
 
 type input struct {
@@ -26,3 +46,18 @@ type input struct {
 	errorLine   string
 	successLine string
 }
+
+// type blueprint struct {
+// 	service    string
+// 	repository string
+// }
+
+// var BlueprintDefault = blueprint{
+// 	service: "package " + input.path + "\n\n" +
+// 		"type " + input.param + " struct {\n " +
+// 		"\n\n}",
+
+// 	repository: "package " + input.path + "\n\n" +
+// 		"type " + input.param + " struct {\n " +
+// 		"\n\n}",
+// }
