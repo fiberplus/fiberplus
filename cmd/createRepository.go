1d66767d9c9f95b123bb6cd33aa6f6a3696fcc55
sachin <sacsand.k@gmail.com>
2021-01-29 17:09:42 +0800

    🚧 test

--- cmd/createRepository.go
+++ cmd/createRepository.go
@@ -133,14 +133,4 @@ var createRepositoryCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(createRepositoryCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// helloCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
