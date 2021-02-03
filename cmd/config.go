c0f232dd5a2071bf4f9401c6bd4cb091876ffc40
sachin <sacsand.k@gmail.com>
2021-02-03 16:23:41 +0800

    ✨  New helper functions

--- cmd/config.go
+++ cmd/config.go
@@ -1,12 +1,31 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 )
+
+var (
+	homeDir string
+
+	execLookPath = exec.LookPath
+	execCommand  = exec.Command
+	osExit       = os.Exit
+
+	skipSpinner bool
+)
+
+func init() {
+	homeDir, _ = os.UserHomeDir()
+}
 
 func check(e error) {
 	if e != nil {
@@ -82,4 +101,67 @@ func (c *config) getConfig() *config {
 	createDirectory(c.PkgPath)
 
 	return c
+}
+
+func runCmd(cmd *exec.Cmd) (err error) {
+
+	var (
+		stderr io.ReadCloser
+		stdout io.ReadCloser
+	)
+
+	if stderr, err = cmd.StderrPipe(); err != nil {
+		return
+	}
+	defer func() {
+		_ = stderr.Close()
+	}()
+	go func() { _, _ = io.Copy(os.Stderr, stderr) }()
+
+	if stdout, err = cmd.StdoutPipe(); err != nil {
+		return
+	}
+	defer func() {
+		_ = stdout.Close()
+	}()
+	go func() { _, _ = io.Copy(os.Stdout, stdout) }()
+
+	if err = cmd.Run(); err != nil {
+		err = fmt.Errorf("failed to run %s", cmd.String())
+	}
+
+	return
+}
+
+// replaces matching file patterns in a path, including subdirectories
+func replace(path, pattern, old, new string) error {
+	return filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		return replaceWalkFn(path, info, pattern, []byte(old), []byte(new))
+	})
+}
+
+func replaceWalkFn(path string, info os.FileInfo, pattern string, old, new []byte) (err error) {
+	var matched bool
+	if matched, err = filepath.Match(pattern, info.Name()); err != nil {
+		return
+	}
+
+	if matched {
+		var oldContent []byte
+		if oldContent, err = ioutil.ReadFile(filepath.Clean(path)); err != nil {
+			return
+		}
+
+		if err = ioutil.WriteFile(path, bytes.Replace(oldContent, old, new, -1), 0); err != nil {
+			return
+		}
+	}
+
+	return
 }
